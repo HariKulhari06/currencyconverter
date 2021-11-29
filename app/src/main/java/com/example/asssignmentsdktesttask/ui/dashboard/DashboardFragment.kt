f10823968cff916e9d45a45b7925d2cfd26e1782
Hari Choudhary <harikulhari06@gmail.com>
2021-11-29 13:59:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/asssignmentsdktesttask/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,31 @@
+package com.example.asssignmentsdktesttask.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.asssignmentsdktesttask.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private val viewModel:DashboardViewModel by viewModels()
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/asssignmentsdktesttask/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.asssignmentsdktesttask.ui.dashboard
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel()
